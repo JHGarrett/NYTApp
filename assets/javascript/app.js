0e3509185938e32e2a349bf5090752527f17abe9
barbi105 <bontiverosg@gmail.com>
2019-03-13 13:25:57 -0500

    started the javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,11 @@
 // api key 29vtt7W1RJuvNa9JG9mhtfGlYQGIa23D
+
+var search = $("#search").val();
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=29vtt7W1RJuvNa9JG9mhtfGlYQGIa23D"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
