59cdf7a06ce655a3e6455386f72540bc7a6204a1
barbi105 <bontiverosg@gmail.com>
2019-03-13 13:47:17 -0500

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 // api key 29vtt7W1RJuvNa9JG9mhtfGlYQGIa23D
+// &begin_date=20120101&end_date=20120101
 
-var search = $("#search").val();
+var search = "sports"
+var number = 20
+var start = "";
+var end = "";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=29vtt7W1RJuvNa9JG9mhtfGlYQGIa23D"
 
 $.ajax({
